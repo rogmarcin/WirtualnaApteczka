19c5630d4ae046fb82e3efe756467c991cd523db
Marcin Rog <marcinrog@hotmail.com>
2018-04-20 21:13:03 +0200

    tesowanie logowania w kontenerze phonegap

---
+++ www/js/firebase.js
@@ -0,0 +1,16 @@
+var app_fireBase = {};
+
+(function() {
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAvDbvPWEHrQmjh2iYZS-7EvRrtE8pTh7Q",
+    authDomain: "wirtualnaapteczka.firebaseapp.com",
+    databaseURL: "https://wirtualnaapteczka.firebaseio.com",
+    projectId: "wirtualnaapteczka",
+    storageBucket: "wirtualnaapteczka.appspot.com",
+    messagingSenderId: "292234828986"
+  };
+  firebase.initializeApp(config);
+    
+  app_fireBase = fireBase;
+})()
