ab733ea4c08590ec5f29589d262636d62fb87997
Mateusz Lis <dev@mateuszlis.com>
2018-04-22 12:31:03 +0200

    login testing

--- www/js/login.js
+++ www/js/login.js
@@ -36,18 +36,3 @@ var uiConfig = {
   // The start method will wait until the DOM is loaded.
     ui.start('#firebaseui-auth-container', uiConfig);
 })()
-
-firebase.auth().signInWithRedirect(provider).then(function() {
-  return firebase.auth().getRedirectResult();
-}).then(function(result) {
-  // This gives you a Google Access Token.
-  // You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-});
