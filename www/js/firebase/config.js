48e4ecc5706dbfc8af2cdc4dd9c4ba02210557fa
Marcin Rog <marcinrog@hotmail.com>
2018-05-02 00:07:29 +0200

    testing

--- www/js/firebase/config.js
+++ www/js/firebase/config.js
@@ -1,12 +1,12 @@
 
     // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyAvDbvPWEHrQmjh2iYZS-7EvRrtE8pTh7Q",
-    authDomain: "wirtualnaapteczka.firebaseapp.com",
-    databaseURL: "https://wirtualnaapteczka.firebaseio.com",
-    projectId: "wirtualnaapteczka",
-    storageBucket: "wirtualnaapteczka.appspot.com",
-    messagingSenderId: "292234828986"
+    apiKey: "AIzaSyBpFvY27M4ci9KaQNtdlys3Z5ybs-u3h-4",
+    authDomain: "login-46957.firebaseapp.com",
+    databaseURL: "https://login-46957.firebaseio.com",
+    projectId: "login-46957",
+    storageBucket: "login-46957.appspot.com",
+    messagingSenderId: "718919480897"
   };
   
   firebase.initializeApp(config);
